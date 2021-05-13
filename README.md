cbd2fe6ed0ca2e9c11d8f71b8d9f7832f73da2a6
Olzhas Suleimen <amajisenpai@gmail.com>
2021-05-13 09:46:16 +0600

    sync 1.0.0-dev.32

--- README.md
+++ README.md
@@ -1,9 +1,99 @@
 piko.dart
 =========
 
-[Svelte](https://svelte.dev) based [React](https://reactjs.com) like framework for [Dart](https://dart.dev).
+[React](https://reactjs.com) looking [Svelte](https://svelte.dev) based framework for [Dart](https://dart.dev).
 
-todo:
-- fragment compiler:
+Plan
+====
+
+Stage 1: able to handle static html with `{ variable }`
+- fragment compiler
   - parser
   - generator
+- dartx compiler
+  - parser
+  - builder
+- VS Code extension
+  - syntax highlighter
+  - language server
+
+Stage 2:
+- event handlers
+- change detector
+- ...
+
+Example
+=======
+from:
+```
+import 'package:piko/internal.dart';
+
+class App extends Component<App> {
+  App({this.name = 'world'});
+  
+  final String name;
+
+  @override
+  Fragment<App> render(RenderTree tree) {
+    return <p id="title">hello { name }!</p>;
+  }
+}
+```
+
+to:
+```
+library app;
+
+import 'dart:html';
+
+import 'package:piko/runtime.dart';
+
+class App extends Component<App> {
+  App({this.name = 'world'});
+
+  final String name;
+
+  @override
+  Fragment<App> render(RenderTree tree) {
+    return AppFragment(this, tree);
+  }
+}
+
+class AppFragment extends Fragment<App> {
+  AppFragment(App context, RenderTree tree) : super(context, tree);
+
+  // `late` is too much here, node `!` checks drops after compiling
+
+  Element? p1;
+
+  Text? t1;
+
+  Text? t2;
+
+  Text? t3;
+
+  @override
+  void create() {
+    p1 = element('p');
+    t1 = text('hello ');
+    t2 = text(context.name);
+    t3 = text('!');
+    attr(p1!, 'id', 'title');
+  }
+
+  @override
+  void mount(Element target, [Node? anchor]) {
+    insert(target, p1!, anchor);
+    append(p1!, t1!);
+    append(p1!, t2!);
+    append(p1!, t3!);
+  }
+
+  @override
+  void detach(bool detaching) {
+    if (detaching) {
+      remove(p1!);
+    }
+  }
+}
+```
--- lib/src/runtime/scheduler.dart
+++ lib/src/runtime/scheduler.dart
@@ -1,54 +1,54 @@
 import 'component.dart';
 
 class Scheduler {
-  Scheduler()
-      : dirtyFragments = <Fragment>[],
-        resolvedFuture = Future<void>.value(),
-        updateScheduled = false,
-        flushing = false;
+  // Scheduler()
+  //     : dirtyFragments = <Fragment>[],
+  //       resolvedFuture = Future<void>.value(),
+  //       updateScheduled = false,
+  //       flushing = false;
 
-  final List<Fragment> dirtyFragments;
+  // final List<Fragment> dirtyFragments;
 
-  Future<void> resolvedFuture;
+  // Future<void> resolvedFuture;
 
-  bool updateScheduled;
+  // bool updateScheduled;
 
-  bool flushing;
+  // bool flushing;
 
-  void makeDirty(Fragment fragment) {
-    dirtyFragments.add(fragment);
-    scheduleUpdate();
-  }
+  // void makeDirty(Fragment fragment) {
+  //   dirtyFragments.add(fragment);
+  //   scheduleUpdate();
+  // }
 
-  void scheduleUpdate() {
-    if (!updateScheduled) {
-      updateScheduled = true;
-      resolvedFuture = resolvedFuture.then<void>((void _) {
-        flush();
-      });
-    }
-  }
+  // void scheduleUpdate() {
+  //   if (!updateScheduled) {
+  //     updateScheduled = true;
+  //     resolvedFuture = resolvedFuture.then<void>((void _) {
+  //       flush();
+  //     });
+  //   }
+  // }
 
-  void flush() {
-    if (flushing) {
-      return;
-    }
+  // void flush() {
+  //   if (flushing) {
+  //     return;
+  //   }
 
-    flushing = true;
+  //   flushing = true;
 
-    do {
-      for (final fragment in dirtyFragments) {
-        update(fragment);
-      }
+  //   do {
+  //     for (final fragment in dirtyFragments) {
+  //       update(fragment);
+  //     }
 
-      dirtyFragments.clear();
-    } while (dirtyFragments.isNotEmpty);
+  //     dirtyFragments.clear();
+  //   } while (dirtyFragments.isNotEmpty);
 
-    updateScheduled = false;
-    flushing = false;
-  }
+  //   updateScheduled = false;
+  //   flushing = false;
+  // }
 
-  void update(Fragment fragment) {
-    fragment.update();
-  }
+  // void update(Fragment fragment) {
+  //   fragment.update();
+  // }
 }
