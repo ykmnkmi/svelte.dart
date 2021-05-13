daaed39d4e8f40b025b49d0c698b42c54b85c378
Olzhas Suleimen <amajisenpai@gmail.com>
2021-05-13 15:11:00 +0600

    sync 1.0.0-dev.34

--- bin/watch.dart
+++ bin/watch.dart
@@ -3,94 +3,100 @@ import 'dart:io';
 
 import 'package:frontend_server_client/frontend_server_client.dart';
 import 'package:path/path.dart' as path;
-import 'package:pedantic/pedantic.dart';
 import 'package:stack_trace/stack_trace.dart';
 import 'package:stream_transform/stream_transform.dart';
 
 const String platformDill = 'lib/_internal/vm_platform_strong.dill';
 
-Future<void> main(List<String> arguments) async {
+void main(List<String> arguments) {
   if (arguments.isEmpty) {
     return;
   }
 
   arguments = arguments.toList();
 
-  try {
-    final sdkRoot = path.dirname(path.dirname(Platform.resolvedExecutable));
-    final outputDillDirectory = Directory.systemTemp.createTempSync('piko');
-    final outputDillFile = File(path.join(outputDillDirectory.path, 'output.dill'));
+  final sdkRoot = path.dirname(path.dirname(Platform.resolvedExecutable));
+  final outputDill = path.join('.dart_tool', 'out', 'output.dill');
 
-    final runAfterCompile = arguments.remove('-r') || arguments.remove('--run');
+  final runAfterCompile = arguments.remove('-r') || arguments.remove('--run');
 
-    final targetFilePath = arguments[0];
-    final targetFile = File(targetFilePath);
+  final targetFilePath = path.normalize(arguments[0]);
+  final targetFile = File(targetFilePath);
 
-    final client = await FrontendServerClient.start(targetFilePath, outputDillFile.path, platformDill, printIncrementalDependencies: false, sdkRoot: sdkRoot);
-
-    ProcessSignal.sigint.watch().listen((_) async {
-      await client.shutdown();
-    });
-
-    if (Platform.isLinux) {
-      ProcessSignal.sigterm.watch().listen((_) async {
-        await client.shutdown();
-      });
+  void start(FrontendServerClient client) {
+    Future<void> run([String line = '']) {
+      final arguments = <String>[outputDill, ...line.trim().split(' ')];
+      final message = 'running ${Platform.resolvedExecutable} ${arguments.join(' ')}';
+      print(message);
+      print('-' * (message.length - 1));
+      return Process.run(Platform.resolvedExecutable, arguments).then<void>(runCompiled).catchError(catchError);
     }
 
-    Future<void> run(String line) async {
-      final arguments = <String>[outputDillFile.path, ...line.trim().split(' ')];
-      stdout.writeln('${Platform.resolvedExecutable} ${arguments.join(' ')}');
-
-      final result = await Process.run(Platform.resolvedExecutable, arguments, runInShell: true);
-      stdout.write(result.stdout);
-    }
-
-    Future<void> compile([FileSystemEvent? event]) async {
-      final result = await client.compile(event == null ? null : <Uri>[targetFile.uri]);
-
-      if (result != null) {
-        if (result.errorCount == 0) {
-          stdout.writeln('compiled successful');
-          result.compilerOutputLines.forEach(stdout.writeln);
-          client.accept();
-          client.reset();
-
-          if (runAfterCompile) {
-            await run('');
-          }
-        } else {
-          stdout.writeln('compiled with ${result.errorCount} error(s)');
-          await client.reject();
+    Future<void> compile([FileSystemEvent? event]) {
+      Future<void> parse(CompileResult? result) {
+        if (result == null) {
+          print('no compilation result, rejecting');
+          return client.reject();
         }
-      } else {
-        stdout.writeln('no compilation result, rejecting');
-        await client.reject();
+
+        if (result.errorCount > 0) {
+          print('compiled with ${result.errorCount} error(s)');
+          return client.reject();
+        }
+
+        print('compiled successful');
+        result.compilerOutputLines.forEach(print);
+        client.accept();
+        client.reset();
+
+        if (runAfterCompile) {
+          return run();
+        }
+
+        return Future<void>.value();
       }
+
+      return client.compile(event == null ? null : <Uri>[targetFile.uri]).then<void>(parse).catchError(catchError);
     }
 
     if (Platform.isWindows) {
       final stream = targetFile.parent.watch(events: FileSystemEvent.modify).where((event) => event.path == targetFile.path);
-      unawaited(stream.debounce(Duration(milliseconds: 100)).asyncMap<void>(compile).drain<void>());
+      stream.debounce(Duration(milliseconds: 100)).asyncMap<void>(compile).drain<void>();
     } else {
       final stream = targetFile.watch(events: FileSystemEvent.modify);
-      unawaited(stream.debounce(Duration(milliseconds: 100)).asyncMap<void>(compile).drain<void>());
+      stream.debounce(Duration(milliseconds: 100)).asyncMap<void>(compile).drain<void>();
     }
 
-    Future<void> listen(String line) async {
+    Future<void> listen(String line) {
       if (line.startsWith('run')) {
-        await run(line.substring(3));
-      } else if (line.startsWith('exit')) {
-        stdout.writeln('shutdown ...');
-        await client.shutdown();
-        exit(0);
+        return run(line.substring(3));
       }
+
+      if (line.startsWith('exit')) {
+        print('shutdown ...');
+        return client.shutdown().then<void>(exit);
+      }
+
+      return Future<void>.value();
     }
 
-    await compile();
-    await stdin.transform(utf8.decoder).asyncMap<void>(listen).drain<void>();
-  } catch (error, stack) {
-    stdout.writeln(error);
-    stdout.writeln(Trace.format(stack));
+    compile().then((_) => stdin.transform(utf8.decoder).asyncMap<void>(listen).drain<void>());
   }
+
+  FrontendServerClient.start(targetFilePath, outputDill, platformDill, printIncrementalDependencies: false, sdkRoot: sdkRoot)
+      .then<void>(start)
+      .catchError(catchError);
+}
+
+void runCompiled(ProcessResult result) {
+  stdout.write(result.stdout);
+
+  if (result.stderr != null) {
+    stdout.write(result.stderr);
+  }
+}
+
+void catchError(Object error, StackTrace stack) {
+  print(error);
+  print(Trace.format(stack));
 }
