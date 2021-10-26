6373a0b0ae6d286ed40f9766fa51200cc8789bc1
Olzhas Suleimen <amajisenpai@gmail.com>
2021-10-26 17:42:50 +0600

    sync 1.0.0-dev.69

--- lib/src/compiler/fragment.dart
+++ lib/src/compiler/fragment.dart
@@ -10,15 +10,15 @@ String compile(String template, {required String sourceUrl, List<String> exports
   return compileNodes(name, nodes);
 }
 
-String compileNodes(String name, List<StandaloneTemplateAst> nodes, {List<String> exports = const <String>[]}) {
+String compileNodes(String name, List<StandaloneTemplate> nodes, {List<String> exports = const <String>[]}) {
   var compiler = FragmentCompiler(name, nodes, exports);
   return compiler.compile();
 }
 
-class FragmentCompiler extends Compiler implements TemplateAstVisitor<String?, String> {
+class FragmentCompiler extends Compiler implements TemplateVisitor<String?, String> {
   FragmentCompiler(String name, this.nodes, [List<String> exports = const <String>[]]) : super(name, exports);
 
-  final List<StandaloneTemplateAst> nodes;
+  final List<StandaloneTemplate> nodes;
 
   @override
   String compile() {
@@ -34,12 +34,12 @@ class FragmentCompiler extends Compiler implements TemplateAstVisitor<String?, S
   }
 
   @override
-  String? visitAnnotation(AnnotationAst astNode, [String? context]) {
+  String? visitAnnotation(Annotation astNode, [String? context]) {
     throw UnimplementedError('visitAnnotation');
   }
 
   @override
-  String? visitAttribute(AttributeAst astNode, [String? context]) {
+  String? visitAttribute(Attribute astNode, [String? context]) {
     var id = context!;
     var attr = astNode.name;
 
@@ -47,27 +47,27 @@ class FragmentCompiler extends Compiler implements TemplateAstVisitor<String?, S
   }
 
   @override
-  String? visitBanana(BananaAst astNode, [String? context]) {
+  String? visitBanana(Banana astNode, [String? context]) {
     throw UnimplementedError('visitBanana');
   }
 
   @override
-  String? visitCloseElement(CloseElementAst astNode, [String? context]) {
+  String? visitCloseElement(CloseElement astNode, [String? context]) {
     throw UnimplementedError('visitCloseElement');
   }
 
   @override
-  String? visitComment(CommentAst astNode, [String? context]) {
+  String? visitComment(Comment astNode, [String? context]) {
     throw UnimplementedError('visitComment');
   }
 
   @override
-  String? visitContainer(ContainerAst astNode, [String? context]) {
+  String? visitContainer(Container astNode, [String? context]) {
     throw UnimplementedError('visitContainer');
   }
 
   @override
-  String? visitElement(ElementAst node, [String? context]) {
+  String? visitElement(Element node, [String? context]) {
     var id = getId(node.name);
     fields.add('late Element $id');
     created.add('$id = element(\'${node.name}\')');
@@ -95,7 +95,7 @@ class FragmentCompiler extends Compiler implements TemplateAstVisitor<String?, S
   }
 
   @override
-  String? visitEmbeddedContent(EmbeddedContentAst astNode, [String? context]) {
+  String? visitEmbeddedContent(EmbeddedContent astNode, [String? context]) {
     throw UnimplementedError('visitEmbeddedContent');
   }
 
@@ -105,13 +105,13 @@ class FragmentCompiler extends Compiler implements TemplateAstVisitor<String?, S
   }
 
   @override
-  String? visitEvent(EventAst astNode, [String? context]) {
+  String? visitEvent(Event astNode, [String? context]) {
     var id = context!;
     listened.add('listen($id, ${astNode.name}, (event) => context.${astNode.value})');
   }
 
   @override
-  String? visitInterpolation(InterpolationAst astNode, [String? context]) {
+  String? visitInterpolation(Interpolation astNode, [String? context]) {
     var id = getId('text');
     fields.add('late Text $id');
     created.add('$id = text(\'${interpolate(astNode)}\')');
@@ -120,27 +120,27 @@ class FragmentCompiler extends Compiler implements TemplateAstVisitor<String?, S
   }
 
   @override
-  String? visitLetBinding(LetBindingAst astNode, [String? context]) {
+  String? visitLetBinding(LetBinding astNode, [String? context]) {
     throw UnimplementedError('visitLetBinding');
   }
 
   @override
-  String? visitProperty(PropertyAst astNode, [String? context]) {
+  String? visitProperty(Property astNode, [String? context]) {
     throw UnimplementedError('visitProperty');
   }
 
   @override
-  String? visitReference(ReferenceAst astNode, [String? context]) {
+  String? visitReference(Reference astNode, [String? context]) {
     throw UnimplementedError('visitReference');
   }
 
   @override
-  String? visitStar(StarAst astNode, [String? context]) {
+  String? visitStar(Star astNode, [String? context]) {
     throw UnimplementedError('visitStar');
   }
 
   @override
-  String? visitText(TextAst astNode, [String? context]) {
+  String? visitText(Text astNode, [String? context]) {
     var id = getId('text');
     var text = astNode.value;
     fields.add('late Text $id');
@@ -149,19 +149,19 @@ class FragmentCompiler extends Compiler implements TemplateAstVisitor<String?, S
     return id;
   }
 
-  static String interpolate(StandaloneTemplateAst node) {
-    if (node is InterpolationAst) {
+  static String interpolate(StandaloneTemplate node) {
+    if (node is Interpolation) {
       return '\${context.${node.value}}';
     }
 
-    if (node is TextAst) {
+    if (node is Text) {
       return node.value;
     }
 
     throw UnimplementedError();
   }
 
-  static String interpolateAll(Iterable<StandaloneTemplateAst> nodes) {
+  static String interpolateAll(Iterable<StandaloneTemplate> nodes) {
     var buffer = StringBuffer();
 
     for (var node in nodes) {
--- lib/src/compiler/parser.dart
+++ lib/src/compiler/parser.dart
@@ -1,6 +1,6 @@
 import 'package:angular_ast/angular_ast.dart' as ast show parse;
 import 'package:angular_ast/angular_ast.dart' hide parse;
 
-List<StandaloneTemplateAst> parse(String template, {required String sourceUrl}) {
-  return ast.parse(template, sourceUrl: sourceUrl).cast<StandaloneTemplateAst>();
+List<StandaloneTemplate> parse(String template, {required String sourceUrl}) {
+  return ast.parse(template, sourceUrl: sourceUrl).cast<StandaloneTemplate>();
 }
