79eb9d15f00e90bbc510cfa8d89ea5fbb4df6cc4
Olzhas Suleimen <amajisenpai@gmail.com>
2023-02-03 14:30:41 +0600

    sync 3.55.1-dev.169

--- web/main.dart
+++ web/main.dart
@@ -2,15 +2,15 @@ import 'dart:html';
 
 import 'package:svelte/svelte.dart';
 
-// import 'examples/introduction/hello_world/app.dart';
-// import 'examples/introduction/dynamic_attributes/app.dart';
-// import 'examples/introduction/styling/app.dart';
-// import 'examples/introduction/nested_components/app.dart';
-// import 'examples/introduction/html_tags/app.dart';
-// import 'examples/reactivity/reactive_assignments/app.dart';
-// import 'examples/reactivity/reactive_declarations/app.dart';
-// import 'examples/reactivity/reactive_statements/app.dart';
-import 'examples/props/declaring_props/app.dart';
+// import 'examples/00_introduction/hello_world/app.dart';
+// import 'examples/00_introduction/dynamic_attributes/app.dart';
+// import 'examples/00_introduction/styling/app.dart';
+// import 'examples/00_introduction/nested_components/app.dart';
+// import 'examples/00_introduction/html_tags/app.dart';
+// import 'examples/01_reactivity/reactive_assignments/app.dart';
+// import 'examples/01_reactivity/reactive_declarations/app.dart';
+// import 'examples/01_reactivity/reactive_statements/app.dart';
+import 'examples/02_props/declaring_props/app.dart';
 
 void main() {
   App(Options(target: document.body));
