bfea114dd9087420b5ef27cd386db0618846a6b1
Olzhas Suleimen <amajisenpai@gmail.com>
2025-03-18 14:53:14 +0500

    sync(builder): 1.0.0-dev.4.

--- svelte_builder/lib/src/formatter.dart
+++ svelte_builder/lib/src/formatter.dart
@@ -1,3 +1,4 @@
 import 'package:dart_style/dart_style.dart';
+import 'package:pub_semver/pub_semver.dart';
 
-final DartFormatter formatter = DartFormatter();
+final DartFormatter formatter = DartFormatter(languageVersion: Version.none);
